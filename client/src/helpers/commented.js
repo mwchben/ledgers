f88a4ccd7d8bf7aaecd54883189764cbcc470d35
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-22 10:12:54 +0300

    routing pkgs and bootstrap

--- client/src/helpers/commented.js
+++ client/src/helpers/commented.js
@@ -1,3 +1,44 @@
-{/* <a className="" href="https://reactjs.org"  target="_blank" rel="noopener noreferrer">
+
+// .......................................::Side Notes::.................................................//
+<a className="" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
     Learn React
-</a> */}
+</a>
+
+
+
+// .......................................::Routing::.................................................//
+
+import { Link } from "react-router-dom";
+
+export default function App() {
+    return (
+        <>
+            <nav>
+                <Link to="/invoices">Invoices</Link> |{" "}
+                <Link to="/expenses">Expenses</Link>
+            </nav>
+        </>
+    );
+}
+
+//while the index.js is
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import App from "./App";
+import Expenses from "expenses";
+import Invoices from "invoices";
+
+const root = ReactDOM.createRoot(
+    document.getElementById("root")
+);
+root.render(
+    <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<App />} />
+            <Route path="expenses" element={<Expenses />} />
+            <Route path="invoices" element={<Invoices />} />
+        </Routes>
+    </BrowserRouter>
+);
+
+// .......................................::Others::.................................................//
