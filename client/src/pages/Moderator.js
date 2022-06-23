9559474d18c53a5a8c1050a977a4c8fe8ed58c1c
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-23 17:49:21 +0300

    last moderator commit

--- client/src/pages/Moderator.js
+++ client/src/pages/Moderator.js
@@ -4,15 +4,24 @@ export default function Moderator() {
     return (
         <>
             <div className="main">
-                <ul class="nav justify-content-center">
-                    <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#"><i class="bi bi-grid-3x3-gap-fill"></i><br />Actions</a>
+                <ul class="nav nav-tabs justify-content-center" id="myTab" role="tablist">
+                    <li className="nav-item" role="presentation">
+                        <a class="nav-link active" id="actions-tab" data-bs-toggle="tab" data-bs-target="#actions-tab-pane" type="button" role="tab" aria-controls="actions-tab-pane" aria-selected="true" href="#">
+                            <i class="bi bi-grid-3x3-gap-fill" />
+                            <br />Actions
+                        </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link "><i class="bi bi-people-fill"></i><br />Voters</a>
+                    <li className="nav-item" role="presentation">
+                        <a class="nav-link" id="voters-tab" data-bs-toggle="tab" data-bs-target="#voters-tab-pane" type="button" role="tab" aria-controls="voters-tab-pane" aria-selected="true" href="#">
+                            <i class="bi bi-people-fill" />
+                            <br />Voters
+                        </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link "><i class="bi bi-123"></i><br />Votes</a>
+                    <li className="nav-item" role="presentation">
+                        <a class="nav-link" id="votes-tab" data-bs-toggle="tab" data-bs-target="#votes-tab-pane" type="button" role="tab" aria-controls="votes-tab-pane" aria-selected="true" href="#">
+                            <i class="bi bi-123" />
+                            <br />Votes
+                        </a>
                     </li>
                 </ul>
             </div>
