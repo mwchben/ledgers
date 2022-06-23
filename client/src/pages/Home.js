4e6748e33edc2be349e2c766509dcd68378d7911
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-23 16:37:18 +0300

    Complete

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,7 +5,7 @@ export default function Home() {
         <>
             <div class="login-wrap">
                 <div class="login-html">
-                    <input id="tab-1" type="radio" name="tab" class="sign-in" checked /><label for="tab-1" class="tab">Sign In</label>
+                    <input id="tab-1" type="radio" name="tab" class="sign-in" checked /><label for="tab-1" class="tab">Log In</label>
                     <input id="tab-2" type="radio" name="tab" class="sign-up" /><label for="tab-2" class="tab">Sign Up</label>
                     <div class="login-form">
                         <div class="sign-in-htm">
@@ -26,16 +26,16 @@ export default function Home() {
                             </div>
                             <div class="hr"></div>
                             <div class="foot-lnk">
-                                <a href="#forgot">Questions?</a>
-                            </div>
-                            <footer>
-                                <ul className='list-unstyled mt-4'>
-                                    <li><i class="bi bi-whatsapp" style={{ fontSize: "2rem", color: "#008000" }}></i></li>
-                                    <li><i class="bi bi-twitter" style={{ fontSize: "2rem", color: "#0000e6" }}></i></li>
-                                    <li><i class="bi bi-envelope-fill" style={{ fontSize: "2rem", color: "#000000" }}></i></li>
-                                </ul>
+                                <footer>
+                                    <ul className='list-unstyled d-flex justify-content-around'>
+                                        <li><a href='#'><i class="bi bi-whatsapp" style={{ fontSize: "2rem", color: "#000000" }}></i></a></li>
+                                        <li><a href='#' ><i class="bi bi-twitter" style={{ fontSize: "2rem", color: "#000000" }}></i></a></li>
+                                        <li><a href='#' ><i class="bi bi-envelope-fill" style={{ fontSize: "2rem", color: "#000000" }}></i></a></li>
+                                    </ul>
 
-                            </footer>
+                                </footer>
+                            </div>
+
                         </div>
                         <div class="sign-up-htm">
                             <div class="group">
@@ -54,17 +54,16 @@ export default function Home() {
                                 <label for="pass" class="label">Email Address</label>
                                 <input id="pass" type="text" class="input" />
                             </div>
+                            <br />
                             <div class="group">
                                 <input type="submit" class="button" value="Sign Up" />
                             </div>
-                            <div class="hr"></div>
-                            <div class="foot-lnk">
-                                <label for="tab-1" >Registered?</label>
-                            </div>
                         </div>
                     </div>
+
                 </div>
             </div>
+
 
         </>
     );
