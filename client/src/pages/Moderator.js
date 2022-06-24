feb723fefc7d95b75ed147b4589425ae6c52befa
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-24 10:25:42 +0300

    moderator page almost complete

--- client/src/pages/Moderator.js
+++ client/src/pages/Moderator.js
@@ -26,6 +26,36 @@ export default function Moderator() {
                 </ul>
             </div>
 
+            <div class="tab-content content" id="myTabContent">
+                <div class="tab-pane fade show active" id="actions-tab-pane" role="tabpanel" aria-labelledby="actions-tab" tabindex="0">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas erat imperdiet sed euismod nisi. Blandit aliquam etiam erat velit. Lectus urna duis convallis convallis tellus. Dui sapien eget mi proin sed. Consequat id porta nibh venenatis cras sed felis eget. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Aliquam etiam erat velit scelerisque in dictum non consectetur a. Sit amet consectetur adipiscing elit pellentesque habitant morbi. Neque convallis a cras semper auctor. Et magnis dis parturient montes nascetur ridiculus mus.
+
+                        Egestas congue quisque egestas diam in arcu. Enim ut tellus elementum sagittis vitae et. Lacus viverra vitae congue eu. Posuere ac ut consequat semper viverra nam libero justo. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Amet cursus sit amet dictum. Orci a scelerisque purus semper eget duis at tellus. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Euismod lacinia at quis risus sed. Aliquam sem fringilla ut morbi tincidunt augue interdum velit. Elit ullamcorper dignissim cras tincidunt lobortis. Sit amet risus nullam eget felis eget nunc lobortis mattis. Urna duis convallis convallis tellus.
+
+                        Cursus turpis massa tincidunt dui ut ornare lectus. Scelerisque felis imperdiet proin fermentum leo vel orci porta non. Nunc sed id semper risus in hendrerit gravida rutrum. Congue quisque egestas diam in arcu cursus euismod. Vitae tortor condimentum lacinia quis vel eros donec. Mauris pharetra et ultrices neque ornare. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Aliquet enim tortor at auctor urna. In iaculis nunc sed augue lacus viverra vitae congue eu. Gravida rutrum quisque non tellus. Gravida in fermentum et sollicitudin ac.
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="voters-tab-pane" role="tabpanel" aria-labelledby="voters-tab" tabindex="0">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas erat imperdiet sed euismod nisi. Blandit aliquam etiam erat velit. Lectus urna duis convallis convallis tellus. Dui sapien eget mi proin sed. Consequat id porta nibh venenatis cras sed felis eget. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Aliquam etiam erat velit scelerisque in dictum non consectetur a. Sit amet consectetur adipiscing elit pellentesque habitant morbi. Neque convallis a cras semper auctor. Et magnis dis parturient montes nascetur ridiculus mus.
+
+                        Egestas congue quisque egestas diam in arcu. Enim ut tellus elementum sagittis vitae et. Lacus viverra vitae congue eu. Posuere ac ut consequat semper viverra nam libero justo. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Amet cursus sit amet dictum. Orci a scelerisque purus semper eget duis at tellus. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Euismod lacinia at quis risus sed. Aliquam sem fringilla ut morbi tincidunt augue interdum velit. Elit ullamcorper dignissim cras tincidunt lobortis. Sit amet risus nullam eget felis eget nunc lobortis mattis. Urna duis convallis convallis tellus.
+
+                        Cursus turpis massa tincidunt dui ut ornare lectus. Scelerisque felis imperdiet proin fermentum leo vel orci porta non. Nunc sed id semper risus in hendrerit gravida rutrum. Congue quisque egestas diam in arcu cursus euismod. Vitae tortor condimentum lacinia quis vel eros donec. Mauris pharetra et ultrices neque ornare. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Aliquet enim tortor at auctor urna. In iaculis nunc sed augue lacus viverra vitae congue eu. Gravida rutrum quisque non tellus. Gravida in fermentum et sollicitudin ac.
+                    </p>
+                </div>
+                <div class="tab-pane fade" id="votes-tab-pane" role="tabpanel" aria-labelledby="votes-tab" tabindex="0">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Egestas erat imperdiet sed euismod nisi. Blandit aliquam etiam erat velit. Lectus urna duis convallis convallis tellus. Dui sapien eget mi proin sed. Consequat id porta nibh venenatis cras sed felis eget. Phasellus egestas tellus rutrum tellus pellentesque eu tincidunt tortor aliquam. Aliquam etiam erat velit scelerisque in dictum non consectetur a. Sit amet consectetur adipiscing elit pellentesque habitant morbi. Neque convallis a cras semper auctor. Et magnis dis parturient montes nascetur ridiculus mus.
+
+                        Egestas congue quisque egestas diam in arcu. Enim ut tellus elementum sagittis vitae et. Lacus viverra vitae congue eu. Posuere ac ut consequat semper viverra nam libero justo. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Amet cursus sit amet dictum. Orci a scelerisque purus semper eget duis at tellus. Est ullamcorper eget nulla facilisi etiam dignissim diam quis. Euismod lacinia at quis risus sed. Aliquam sem fringilla ut morbi tincidunt augue interdum velit. Elit ullamcorper dignissim cras tincidunt lobortis. Sit amet risus nullam eget felis eget nunc lobortis mattis. Urna duis convallis convallis tellus.
+
+                        Cursus turpis massa tincidunt dui ut ornare lectus. Scelerisque felis imperdiet proin fermentum leo vel orci porta non. Nunc sed id semper risus in hendrerit gravida rutrum. Congue quisque egestas diam in arcu cursus euismod. Vitae tortor condimentum lacinia quis vel eros donec. Mauris pharetra et ultrices neque ornare. Vulputate odio ut enim blandit volutpat maecenas volutpat blandit. Aliquet enim tortor at auctor urna. In iaculis nunc sed augue lacus viverra vitae congue eu. Gravida rutrum quisque non tellus. Gravida in fermentum et sollicitudin ac.
+                    </p>
+                </div>
+            </div>
+
         </>
     );
 };
