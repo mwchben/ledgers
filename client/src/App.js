98f4e4861e79ec803bfc6212d645b835f06aa114
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-27 19:01:23 +0300

    landing page and as a voter or candidate

--- client/src/App.js
+++ client/src/App.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 // import Home from './pages/Home'
 // import Candidate from "./pages/Candidate";
 import Student from "./pages/Student";
@@ -7,11 +8,16 @@ function App() {
   return (
     <>
       <div className="App">
+        <nav>
+          <h3> I am a </h3>
+          <Link to="/logCandidate">Candidate</Link> |{" "}
+          <Link to="/logVoter">Voter</Link>
+        </nav>
 
         {/* < Home /> */}
         {/* <Moderator /> */}
         {/* < Candidate /> */}
-        <Student />
+        {/* <Student /> */}
       </div>
     </>
 
