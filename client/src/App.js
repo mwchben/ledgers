819ec1532d35d1215cf884b54410e951de9ba87b
Mwangi Benjamin <90389018+mwchben@users.noreply.github.com>
2022-06-27 20:15:13 +0300

    server: re-name and cleanup  client: configure routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,18 @@
 import { Link } from "react-router-dom";
-// import Home from './pages/Home'
-// import Candidate from "./pages/Candidate";
+
 import Student from "./pages/Student";
-// import Moderator from "./pages/Moderator";
+
 
 function App() {
   return (
     <>
       <div className="App">
         <nav>
-          <h3> I am a </h3>
+          <h3> I am a</h3>
           <Link to="/logCandidate">Candidate</Link> |{" "}
           <Link to="/logVoter">Voter</Link>
         </nav>
 
-        {/* < Home /> */}
-        {/* <Moderator /> */}
-        {/* < Candidate /> */}
-        {/* <Student /> */}
       </div>
     </>
 
