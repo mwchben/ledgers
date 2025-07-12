83ab7dfafd5197beba6ee15f285db37ca6919de6
Benjamin Mwangi <benjamwa05@gmail.com>
2025-07-12 18:55:18 +0300

    dashboard as opposed to index.html

--- app.py
+++ app.py
@@ -4,11 +4,11 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html', title='Home Page')
+    return render_template('dashboard.html', title='Dashboard')
 
 @app.route("/login")
 def login():
-    return render_template('login.html', title='Login Page')
+    return render_template('login.html', title='login')
 
 if __name__ == "__main__":
     app.run(debug=True)
