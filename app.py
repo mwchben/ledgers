0386f96ab55ae97581ad8b31e6d406df7835e3d0
Benjamin Mwangi <benjamwa05@gmail.com>
2025-07-16 18:09:13 +0300

    route add

--- app.py
+++ app.py
@@ -10,5 +10,9 @@ def index():
 def login():
     return render_template('login.html', title='login')
 
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('page_not_found.html',title='Ooops!'), 404
+
 if __name__ == "__main__":
     app.run(debug=True)
