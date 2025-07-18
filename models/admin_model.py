7d43a20cbbc0447c228e792b38a106dd48ad21b4
Benjamin Mwangi <benjamwa05@gmail.com>
2025-07-18 12:28:59 +0300

    admin model to create 1st 'logger'

---
+++ models/admin_model.py
@@ -0,0 +1,13 @@
+from app import db
+
+class Admin(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(200), nullable=False) 
+
+    # def __init__(self, username, password):
+    #     self.username = username
+    #     self.password = password
+
+    # def __repr__(self):
+    #     return f"<User(username='{self.username}')>"
