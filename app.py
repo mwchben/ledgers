2491c3e75111ac0a6790ae99de99c83802879aba
Benjamin Mwangi <benjamwa05@gmail.com>
2025-07-18 12:36:22 +0300

    password_harsh not needed

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask,render_template,request, redirect, url_for, flash
 from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
-from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 
 
