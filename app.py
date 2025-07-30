efcca218e8785cfb5a78f2266f5b9ebbc61374bd
Benjamin Mwangi <benjamwa05@gmail.com>
2025-07-30 17:05:08 +0300

    werkzeug.exceptions.BadRequestKeyError

--- app.py
+++ app.py
@@ -38,14 +38,14 @@ def login():
     if current_user.is_authenticated:
         return redirect(url_for('dashboard'))
     if request.method == 'POST': 
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form.get['username']
+        password = request.form.get['password']
 
         admin = Admin.query.filter_by(username=username).first()
         if admin and admin.password == password:
                 login_user(admin)
                 flash('Logged in successfully!', 'success')
-                return redirect(url_for('/dashboard'))
+                return redirect(url_for('dashboard'))
         else:
             flash('Invalid username or password', 'danger')
     return render_template('login.html', title='login')
